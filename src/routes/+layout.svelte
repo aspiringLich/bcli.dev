765a0e21a78ac5c6c24506f097f774ddf2e8a36f
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-04-05 21:54:54 -0400

    basic lined paper idea

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 	import '../app.css';
-	
+
 	let { children } = $props();
 </script>
 
-{@render children()}
+<div class="flex flex-col justify-stretch items-center h-full grow">
+	<main class="h-full prose grow my-4 max-w-2xl">
+		{@render children()}
+	</main>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,19 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<h2>Introduction</h2>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+<p>
+	Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab quis labore placeat veritatis
+	repudiandae ad. Minima voluptatibus quia ratione harum consequuntur ducimus dignissimos
+	voluptates, voluptatem enim amet cumque, nemo culpa impedit voluptatum velit. Culpa, commodi fugit
+	cum cumque dolor odit perspiciatis libero ut itaque quia sint suscipit dolorum mollitia delectus
+	saepe quos reiciendis ea aut doloremque quidem accusamus? Laborum animi quia rem placeat eum fuga
+	dolor temporibus nostrum earum! Itaque aliquam neque ut dolor impedit totam voluptas, nobis
+	architecto voluptatem repellendus necessitatibus facilis assumenda molestiae eius velit? Delectus
+	atque harum praesentium ratione quas? Corporis consequuntur, et eligendi ducimus facere omnis?
+</p>
+<p>
+	Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation
+</p>
+
