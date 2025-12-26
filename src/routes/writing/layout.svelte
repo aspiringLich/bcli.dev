87511d85ea377b9ec51fa40f002d6dc8fc6836f5
Brandon Li <48413902+aspiringLich@users.noreply.github.com>
2025-12-26 01:20:17 -0500

    redo layout a bit

--- src/routes/writing/layout.svelte
+++ src/routes/writing/layout.svelte
@@ -26,9 +26,9 @@
 		const updateActiveSection = () => {
 			const sections = outline.map((item) => document.getElementById(item.id)).filter(Boolean);
 			const scrollPosition = window.scrollY + window.innerHeight / 3;
-			
+
 			// if were at the bottom, set all sections as past
-			if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
+			if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
 				activeId = null;
 				pastIds = new Set(sections.map((sec) => sec!.id));
 				return;
@@ -66,83 +66,10 @@
 </script>
 
 <main class="relative">
-	<header class="flex items-center pt-20 gap-4">
-		<hr class="grow border-2 !border-surface-400"/>
-		<h1 class="!m-0">{title}</h1>
-		<hr class="grow border-2 !border-surface-400"/>
-	</header>
-	<aside class="select-none">
-		<div class="py-2 pl-6 rounded-l-full bg-surface-200 italic text-nowrap">
-			{readingTime.text}
-			<br />
-			{readingTime.words} words
-		</div>
-		{#if outline.length > 0}
-			<nav class="mt-4 sticky top-8">
-				<span class="font-semibold">Outline</span>
-				<ul class="!m-0 !mt-2 !pl-0">
-					{#each outline as item}
-						<li class:past={pastIds.has(item.id)} class:reading={activeId === item.id}>
-							<a href={'#' + item.id} class="text-primary hover:underline">{@html item.html}</a>
-						</li>
-					{/each}
-				</ul>
-			</nav>
-		{/if}
-	</aside>
+	<h1>{title}</h1>
+	<p class="text-italic font-rounded font-semibold text-surface-600 !mt-0">
+		{readingTime.text}
+	</p>
 	{@render children()}
 </main>
 
-<style lang="postcss">
-	@import 'tailwindcss';
-	
-	aside {
-		@apply xl:!-right-10 z-20;
-	}
-
-	nav li {
-		@apply list-none border-l-4 !m-0 !p-0;
-		border-color: var(--color-surface-400);
-		transition:
-			border-color,
-			color,
-			background-color 0.3s;
-		color: var(--color-surface-400);
-		background-color: var(--color-surface-200);
-
-		a {
-			@apply block py-1 pl-2  xl:border-r-2;
-			border-color: var(--color-surface-400);
-			color: inherit !important;
-		}
-
-		&:first-of-type {
-			@apply rounded-tl-sm xl:rounded-tr-sm;
-			a {
-				@apply rounded-tr-sm;
-			}
-		}
-		&:last-of-type {
-			@apply rounded-bl-sm xl:rounded-br-sm;
-			a {
-				@apply rounded-br-sm;
-			}
-		}
-
-		&:hover {
-			border-color: var(--color-accent-500) !important;
-			color: var(--color-accent-500) !important;
-		}
-
-		&.past {
-			border-color: var(--color-accent-500);
-			color: var(--color-surface-500);
-		}
-
-		&.reading {
-			border-color: var(--color-accent-500);
-			color: var(--color-accent-500);
-			background-color: var(--color-accent-200);
-		}
-	}
-</style>
